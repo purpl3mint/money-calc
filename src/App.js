c833ca800c477468a7730c2a2e27b80de32232e1
Dmitry <purpl3mint@yandex.ru>
2020-08-17 16:11:48 +0700

    React project of money calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Total from "./components/total/Total";
+import History from "./components/history/History";
+import Operation from "./components/operation/Operation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    transactions: JSON.parse(localStorage.getItem("calcMoney1")) || [],
+    description: "",
+    amount: "",
+    resultIncome: 0,
+    resultExpenses: 0,
+    totalBalance: 0,
+  };
+
+  componentWillMount() {
+    this.getTotalBalance();
+  }
+
+  componentWillUpdate() {
+    this.addToStorage();
+  }
+
+  addTransaction = (add) => {
+    const transactions = [...this.state.transactions];
+
+    const transaction = {
+      id: `cmr${(+new Date()).toString(16)}`,
+      description: this.state.description,
+      amount: parseFloat(this.state.amount),
+      add,
+    };
+
+    transactions.push(transaction);
+
+    this.setState(
+      { transactions, description: "", amount: "" },
+      this.getTotalBalance()
+    );
+  };
+
+  addDescription = (e) => {
+    this.setState({ description: e.target.value });
+  };
+
+  addAmount = (e) => {
+    this.setState({ amount: e.target.value });
+  };
+
+  getIncome = () => {
+    return this.state.transactions
+      .filter((item) => item.add)
+      .reduce((acc, item) => acc + item.amount, 0);
+  };
+
+  getExpenses = () => {
+    return this.state.transactions
+      .filter((item) => !item.add)
+      .reduce((acc, item) => acc + item.amount, 0);
+  };
+
+  getTotalBalance = () => {
+    const resultIncome = this.getIncome();
+    const resultExpenses = this.getExpenses();
+
+    const totalBalance = resultIncome - resultExpenses;
+
+    this.setState({
+      resultIncome,
+      resultExpenses,
+      totalBalance,
+    });
+  };
+
+  addToStorage() {
+    localStorage.setItem("calcMoney1", JSON.stringify(this.state.transactions));
+  }
+
+  delTransaction = (key) => {
+    const transactions = this.state.transactions.filter(
+      (item) => item.id !== key
+    );
+    this.setState({ transactions }, this.getTotalBalance);
+  };
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Кошелек</h1>
+          <h2>Калькулятор расходов</h2>
+        </header>
+
+        <main>
+          <div className="container">
+            <Total
+              resultExpenses={this.state.resultExpenses}
+              resultIncome={this.state.resultIncome}
+              totalBalance={this.state.totalBalance}
+            />
+
+            <History
+              transactions={this.state.transactions}
+              delTransaction={this.delTransaction}
+            />
+
+            <Operation
+              addTransaction={this.addTransaction}
+              addDescription={this.addDescription}
+              addAmount={this.addAmount}
+              description={this.state.description}
+              amount={this.state.amount}
+            />
+          </div>
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/history/HistoryItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const HistoryItem = ({ transaction, delTransaction }) => {
+  return (
+    <li
+      className={`history__item ${
+        transaction.add ? "history__item-plus" : "history__item-minus"
+      }`}
+    >
+      {transaction.description}
+      <span className="history__money">{transaction.amount} ₽</span>
+      <button
+        className="history__delete"
+        onClick={() => delTransaction(transaction.id)}
+      >
+        x
+      </button>
+    </li>
+  );
+};
+
+export default HistoryItem;
