a0dac7c59b75245706f265a4bf25f5745bf60886
Dmitry <purpl3mint@yandex.ru>
2020-08-17 16:26:26 +0700

    Updates

---
+++ precache-manifest.db9b97491f4e9d35a051f2c3bb7c9e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a5d1aafea1cc21881c1ea82ae3485d",
+    "url": "/money-calc/index.html"
+  },
+  {
+    "revision": "4648d312cdcf429efe7c",
+    "url": "/money-calc/static/css/main.9e034465.chunk.css"
+  },
+  {
+    "revision": "b154c75e4a189bfc4a4b",
+    "url": "/money-calc/static/js/2.9aa87a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-calc/static/js/2.9aa87a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4648d312cdcf429efe7c",
+    "url": "/money-calc/static/js/main.8b8f3912.chunk.js"
+  },
+  {
+    "revision": "611656e889aaf6deccf9",
+    "url": "/money-calc/static/js/runtime-main.b91bbaa2.js"
+  }
+]);
